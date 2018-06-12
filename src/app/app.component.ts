ce4f8b909332f653f0916f2c28251f77e36165ff
fauchere <fauchere@adobe.com>
2018-06-12 14:50:59 +0200

    @releng creation of the development branch (RTC ok)
    - undefined/unused import
    - version alignment with the available artifacts
    - version scripts
    - adobe dependency update
    - automatic dependency vulnerability update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Constants } from "@adobe/cq-angular-editable-components";
+
 @Component({
   selector: 'aem-root',
   templateUrl: './app.component.html',
