b336e5cc815a8eab8f6f95be39c20eda8421efa4
Niek Raaijmakers <niek@adobe.com>
2020-08-01 12:43:48 +0200

    Fix critical error

--- examples/angular-spa/src/app/app.component.ts
+++ examples/angular-spa/src/app/app.component.ts
@@ -7,6 +7,18 @@ import {CarouselV1Component} from "@adobe/aem-core-components-angular-spa/contai
 import {TabsV1Component} from "@adobe/aem-core-components-angular-spa/containers/tabs/v1";
 import {ContainerV1Component} from "@adobe/aem-core-components-angular-spa/containers/container/v1";
 
+import {TitleV2Component,TitleV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/title/v2';
+import {BreadCrumbV2Component,BreadCrumbV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/layout/breadcrumb/v2';
+import {TextV2Component,TextV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/text/v2';
+import {NavigationV1Component} from  '@adobe/aem-core-components-angular-base/layout/navigation/v1';
+import {ButtonV1Component,ButtonV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/button/v1';
+import {ImageV2Component,ImageV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/image/v2';
+import {LanguageNavigationV1Component} from '@adobe/aem-core-components-angular-base/layout/language-navigation/v1';
+import {TeaserV1Component,TeaserV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/teaser/v1';
+import {DownloadV1Component,DownloadV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/download/v1';
+import {SeparatorV1Component,SeparatorV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/separator/v1';
+import {ListV2Component,ListV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/list/v2';
+
 
 @Component({
   selector: 'app-root',
@@ -20,18 +32,18 @@ export class AppComponent {
 }
 
 
-// MapTo('core-components-examples/wcm/angular/components/navigation')(withComponentMappingContext(NavigationV1));
-// MapTo('core-components-examples/wcm/angular/components/download')(DownloadV1, {isEmpty: DownloadV1IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/languagenavigation')(withComponentMappingContext(LanguageNavigationV1));
-// MapTo('core-components-examples/wcm/angular/components/list')(ListV2, {isEmpty: ListV2IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/separator')(SeparatorV1, {isEmpty: SeparatorV1IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/download')(DownloadV1, {isEmpty: DownloadV1IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/text')(TextV2, {isEmpty: TextV2IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/breadcrumb')(withComponentMappingContext(BreadCrumbV2), {isEmpty: BreadCrumbV2IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/button')(ButtonV1, {isEmpty: ButtonV1IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/teaser')(TeaserV1, {isEmpty: TeaserV1IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/image')(ImageV2, {isEmpty: ImageV2IsEmptyFn});
-// MapTo('core-components-examples/wcm/angular/components/title')(TitleV2, {isEmpty: TitleV2IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/navigation')(NavigationV1Component);
+MapTo('core-components-examples/wcm/angular/components/download')(DownloadV1Component, {isEmpty: DownloadV1IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/languagenavigation')(LanguageNavigationV1Component);
+MapTo('core-components-examples/wcm/angular/components/list')(ListV2Component, {isEmpty: ListV2IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/separator')(SeparatorV1Component, {isEmpty: SeparatorV1IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/download')(DownloadV1Component, {isEmpty: DownloadV1IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/text')(TextV2Component, {isEmpty: TextV2IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/breadcrumb')(BreadCrumbV2Component, {isEmpty: BreadCrumbV2IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/button')(ButtonV1Component, {isEmpty: ButtonV1IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/teaser')(TeaserV1Component, {isEmpty: TeaserV1IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/image')(ImageV2Component, {isEmpty: ImageV2IsEmptyFn});
+MapTo('core-components-examples/wcm/angular/components/title')(TitleV2Component, {isEmpty: TitleV2IsEmptyFn});
 
 
 MapTo('core-components-examples/wcm/angular/components/tabs')(TabsV1Component);
